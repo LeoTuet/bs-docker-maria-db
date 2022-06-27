667666e66e0edd1b7c664db7d83949a3fe743802
Leopold Tueting <leopold@tueting.net>
2022-06-27 13:43:18 +0200

    adds initial java class for maria db connection

---
+++ src/main/java/TestConnection.java
@@ -0,0 +1,16 @@
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TestConnection {
+    public static void main(final String[] args) throws SQLException {
+        final Connection connection = DriverManager.getConnection("jbc:mariadb://xxx");
+
+        final DatabaseMetaData meta = connection.getMetaData();
+
+        System.out.println(meta.getDatabaseProductName());
+
+        System.out.println("hier");
+    }
+}
